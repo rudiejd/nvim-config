6841601e5526f12a09699de339fb9ba99fd4712e
JD Rudie <j.rudie@draftkings.com>
2023-11-26 16:36:09 -0500

    fix: lualine uses theme colors

--- lua/rudiejd/plugins/lualine.lua
+++ lua/rudiejd/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     opts = {
         options = {
             icons_enabled = false,
-            theme = 'onedark',
+            theme = 'base16',
             component_separators = '|',
             section_separators = '',
         },
