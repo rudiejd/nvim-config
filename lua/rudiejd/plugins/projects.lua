9ab9ac45c0de0cd648b7bd37c44e908969a63bb3
JD Rudie <rudiejd@miamioh.edu>
2023-12-29 02:13:57 -0500

    fix: update projects configuration

--- lua/rudiejd/plugins/projects.lua
+++ lua/rudiejd/plugins/projects.lua
@@ -1,5 +1,9 @@
 return {
     "ahmedkhalf/project.nvim",
+    opts = {
+        detection_methods = {"pattern"},
+        patterns = { ".git" }
+    },
     config =function ()
         require('project_nvim').setup({})
     end
