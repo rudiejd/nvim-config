05a89edf77be44f053c9202d0f0a6a80555be634
JD Rudie <rudiejd@miamioh.edu>
2024-01-02 20:48:08 -0500

    feat(mason): this should actually make auto lsp setup work. havent tested tho

--- lua/rudiejd/plugins/mason.lua
+++ lua/rudiejd/plugins/mason.lua
@@ -1,19 +1,20 @@
-local lsp_zero = require('lsp-zero')
-
 return {
-    {"williamboman/mason.nvim",
-        config = function ()
+    {
+        "williamboman/mason.nvim",
+        config = function()
             require('mason').setup()
         end
     },
-    {"williamboman/mason-lspconfig.nvim",
-        ensure_installed = {"lua_ls", "rust_analyzer", "omnisharp", "netcoredbg"},
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {"williamboman/mason.nvim", 'VonHeikemen/lsp-zero.nvim'},
+        ensure_installed = { "lua_ls", "rust_analyzer", "csharp-ls", "netcoredbg" },
         handlers = {
-            lsp_zero.default_setup
+            require('lsp-zero').default_setup
         },
         automatic_installation = true,
-        config = function ()
-           require('mason-lspconfig').setup()
+        config = function()
+            require('mason-lspconfig').setup()
         end
     }
 }
