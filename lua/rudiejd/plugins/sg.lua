ddafa68e2c1b66f1780b4bb26f6724b2f46f6897
JD Rudie <j.rudie@draftkings.com>
2024-02-05 16:17:20 -0500

    feat: add sourceraph back with cookie from env variable

--- lua/rudiejd/plugins/sg.lua
+++ lua/rudiejd/plugins/sg.lua
@@ -1,11 +1,15 @@
 return {
-  -- "sourcegraph/sg.nvim",
-  -- dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-telescope/telescope.nvim",
-  -- },
-  -- dev = true,
-  -- config = function()
-  --     require("sg").setup({})
-  -- end,
+  "sourcegraph/sg.nvim",
+  dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+  },
+  branch = "custom-headers",
+  config = function()
+      require("sg").setup({
+          src_headers = {
+              Cookie = os.getenv("SRC_COOKIE")
+          }
+      })
+  end,
 }
