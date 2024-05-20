bcc41aa16593f933b9314c8f3b70dee12f3bec05
JD Rudie <j.rudie@draftkings.com>
2024-05-20 19:33:47 -0400

    fix: dap

--- lua/rudiejd/plugins/nvim-dap.lua
+++ lua/rudiejd/plugins/nvim-dap.lua
@@ -201,7 +201,7 @@ return {
     'theHamsta/nvim-dap-virtual-text',
     dependencies = {'mfussenegger/nvim-dap' },
     config = function()
-      require('dap-virtual-text').setup()
+      require('nvim-dap-virtual-text').setup()
     end,
   }
 }
