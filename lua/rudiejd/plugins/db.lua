743302277c355e934e13cebf4a82b73a05807b4a
JD Rudie <j.rudie@draftkings.com>
2024-11-07 15:11:58 -0500

    feat: add dbee.nvim

---
+++ lua/rudiejd/plugins/db.lua
@@ -0,0 +1,23 @@
+return {
+  "kndndrj/nvim-dbee",
+  dev = true,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  build = function()
+    -- Install tries to automatically detect the install method.
+    -- if it fails, try calling it with one of these parameters:
+    --    "curl", "wget", "bitsadmin", "go"
+    require("dbee").install()
+  end,
+  config = function()
+    require("dbee").setup {
+      sources = {
+        require("dbee.sources").MemorySource:new({
+          {
+          },
+          -- ...
+        })
+      } }
+  end,
+}
