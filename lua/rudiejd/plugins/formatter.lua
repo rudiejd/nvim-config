af0e884349b10fdc8a3d99a0587c8aa884db4686
JD <jd@rudiejd.io>
2025-03-26 12:01:41 -0400

    fix(formatter): add ruff formatter for python

--- lua/rudiejd/plugins/formatter.lua
+++ lua/rudiejd/plugins/formatter.lua
@@ -3,13 +3,12 @@ return {
     'stevearc/conform.nvim',
     opts = {},
     config = function()
-
       require('conform').setup({
         formatters_by_ft = {
-
           lua = { 'stylua' },
           c = { 'clang-format' },
-          cpp = { 'clang-format' }
+          cpp = { 'clang-format' },
+          python = { 'ruff' }
         },
         format_on_save = {
           timeout_ms = 500,
