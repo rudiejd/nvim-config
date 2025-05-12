4dc01e51ad68a13aca27066add33a72aab8e90f8
JD <jd@rudiejd.io>
2025-05-12 11:30:03 -0400

    fixup: remove mix as elixir formatter

--- lua/rudiejd/plugins/formatter.lua
+++ lua/rudiejd/plugins/formatter.lua
@@ -3,21 +3,19 @@ return {
     'stevearc/conform.nvim',
     opts = {},
     config = function()
-      require('conform').setup({
+      require('conform').setup {
         formatters_by_ft = {
           lua = { 'stylua' },
           c = { 'clang-format' },
           cpp = { 'clang-format' },
           python = { 'ruff' },
           terraform = { 'tofu_fmt' },
-          elixir = { 'mix' }
-
         },
         format_on_save = {
           timeout_ms = 500,
-          lsp_format = "fallback"
-        }
-      })
-    end
-  }
+          lsp_format = 'fallback',
+        },
+      }
+    end,
+  },
 }
