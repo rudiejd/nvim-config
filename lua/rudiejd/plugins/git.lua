883087327e9e28f41f3ad7db3b0507bd66ff341b
JD <jd@rudiejd.io>
2025-11-13 21:19:17 -0500

    chore: update all plugins. had to remove some telescope extensions and disable gitsigns for now

--- lua/rudiejd/plugins/git.lua
+++ lua/rudiejd/plugins/git.lua
@@ -12,56 +12,56 @@ return {
       vim.g.fubitive_domain_pattern = 'bitbucket.build.dkinternal.com'
     end,
   },
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        vim.keymap.set(
-          'n',
-          '<leader>gp',
-          require('gitsigns').preview_hunk,
-          { buffer = bufnr, desc = '[G]it [P]review' }
-        )
-        vim.keymap.set(
-          'n',
-          '<leader>gr',
-          require('gitsigns').reset_hunk,
-          { buffer = bufnr, desc = '[G]it [R]eset Hunk' }
-        )
-        vim.keymap.set('n', '<leader>gd', require('gitsigns').diffthis, { buffer = bufnr, desc = '[G]it [D]iff' })
-
-        -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
-        vim.keymap.set({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-        vim.keymap.set({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-      end,
-    },
-  },
+  -- {
+  --   -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   'lewis6991/gitsigns.nvim',
+  --   opts = {
+  --     -- See `:help gitsigns.txt`
+  --     signs = {
+  --       add = { text = '+' },
+  --       change = { text = '~' },
+  --       delete = { text = '_' },
+  --       topdelete = { text = '‾' },
+  --       changedelete = { text = '~' },
+  --     },
+  --     on_attach = function(bufnr)
+  --       vim.keymap.set(
+  --         'n',
+  --         '<leader>gp',
+  --         require('gitsigns').preview_hunk,
+  --         { buffer = bufnr, desc = '[G]it [P]review' }
+  --       )
+  --       vim.keymap.set(
+  --         'n',
+  --         '<leader>gr',
+  --         require('gitsigns').reset_hunk,
+  --         { buffer = bufnr, desc = '[G]it [R]eset Hunk' }
+  --       )
+  --       vim.keymap.set('n', '<leader>gd', require('gitsigns').diffthis, { buffer = bufnr, desc = '[G]it [D]iff' })
+  --
+  --       -- don't override the built-in and fugitive keymaps
+  --       local gs = package.loaded.gitsigns
+  --       vim.keymap.set({ 'n', 'v' }, ']c', function()
+  --         if vim.wo.diff then
+  --           return ']c'
+  --         end
+  --         vim.schedule(function()
+  --           gs.next_hunk()
+  --         end)
+  --         return '<Ignore>'
+  --       end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+  --       vim.keymap.set({ 'n', 'v' }, '[c', function()
+  --         if vim.wo.diff then
+  --           return '[c'
+  --         end
+  --         vim.schedule(function()
+  --           gs.prev_hunk()
+  --         end)
+  --         return '<Ignore>'
+  --       end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+  --     end,
+  --   },
+  -- },
   {
     'sindrets/diffview.nvim',
     config = function ()
--- lua/rudiejd/plugins/telescope.lua
+++ lua/rudiejd/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
   'nvim-telescope/telescope.nvim',
-  -- dev = true,
+  tag = '0.1.9',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
@@ -11,25 +11,19 @@ return {
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
-    },
-    'benfowler/telescope-luasnip.nvim',
+    }
   },
   config = function()
-    require('telescope').load_extension 'luasnip'
-    require('telescope').load_extension 'projects'
-
     local builtin = require 'telescope.builtin'
     local telescope = require 'telescope'
 
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sp', builtin.git_files, { desc = '[S]earch [P]roject' })
-    vim.keymap.set('n', '<leader>sP', telescope.extensions.projects.projects, { desc = '[S]earch [P]rojectS' })
     vim.keymap.set('n', '<leader>st', builtin.live_grep, { desc = '[S]earch [T]ext' })
     vim.keymap.set('n', '<Leader>sr', builtin.lsp_references, { desc = '[S]earch [R]eferences' })
     vim.keymap.set('n', '<Leader>sR', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sx', builtin.diagnostics, { desc = '[S]earch E[x]ceptions' })
-    vim.keymap.set('n', '<leader>ss', telescope.extensions.luasnip.luasnip, { desc = '[S]earch [S]nippets' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sa', function()
       builtin.builtin { include_extensions = true }
