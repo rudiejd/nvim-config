0927f5fb362a883987b1ca1a66fc48b7097dc9a4
JD <jd@rudiejd.io>
2025-12-02 16:20:26 -0500

    chore: winter cleaning! remove all the shit i'm not using

--- lua/rudiejd/plugins/neofsharp.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'ionide/Ionide-vim',
-  ft = 'fsharp',
-}
--- lua/rudiejd/plugins/nvim-coverage.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'andythigpen/nvim-coverage',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('coverage').setup()
-  end
-}
