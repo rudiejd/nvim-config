49030bab1e9e51dcf1788303fdab26c25f566315
JD <jd@rudiejd.io>
2026-05-19 14:27:15 -0400

    chore: upgrade treesitter

--- lua/rudiejd/plugins/treesitter.lua
+++ lua/rudiejd/plugins/treesitter.lua
@@ -1,90 +1,91 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
     config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'vim', 'vimdoc', 'luadoc', 'lua', 'c_sharp', 'javascript', 'rust' },
-        auto_install = true,
+      require('nvim-treesitter').setup {
 
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = true,
         },
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
 
-        textobjects = {
-          select = {
-            enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-              -- You can also use captures from other query groups like `locals.scm`
-              ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V', -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true of false
-            include_surrounding_whitespace = true,
-          },
-        },
+        -- textobjects = {
+        --   select = {
+        --     enable = true,
+        --
+        --     -- Automatically jump forward to textobj, similar to targets.vim
+        --     lookahead = true,
+        --
+        --     keymaps = {
+        --       -- You can use the capture groups defined in textobjects.scm
+        --       ['af'] = '@function.outer',
+        --       ['if'] = '@function.inner',
+        --       ['ac'] = '@class.outer',
+        --       -- You can optionally set descriptions to the mappings (used in the desc parameter of
+        --       -- nvim_buf_set_keymap) which plugins like which-key display
+        --       ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+        --       -- You can also use captures from other query groups like `locals.scm`
+        --       ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
+        --     },
+        --     -- You can choose the select mode (default is charwise 'v')
+        --     --
+        --     -- Can also be a function which gets passed a table with the keys
+        --     -- * query_string: eg '@function.inner'
+        --     -- * method: eg 'v' or 'o'
+        --     -- and should return the mode ('v', 'V', or '<c-v>') or a table
+        --     -- mapping query_strings to modes.
+        --     selection_modes = {
+        --       ['@parameter.outer'] = 'v', -- charwise
+        --       ['@function.outer'] = 'V', -- linewise
+        --       ['@class.outer'] = '<c-v>', -- blockwise
+        --     },
+        --     -- If you set this to `true` (default is `false`) then any textobject is
+        --     -- extended to include preceding or succeeding whitespace. Succeeding
+        --     -- whitespace has priority in order to act similarly to eg the built-in
+        --     -- `ap`.
+        --     --
+        --     -- Can also be a function which gets passed a table with the keys
+        --     -- * query_string: eg '@function.inner'
+        --     -- * selection_mode: eg 'v'
+        --     -- and should return true of false
+        --     include_surrounding_whitespace = true,
+        --   },
+        -- },
       }
-
-      local parsers = require('nvim-treesitter.parsers')
-
-      local parser_config = parsers.get_parser_configs()
-      print(vim.inspect(parser_config.xml.filetype_to_parsername))
 
       vim.treesitter.language.register('starlark', 'tiltfile')
     end,
   },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    config = function()
-      require('treesitter-context').setup {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      }
-    end,
-  },
+  -- {
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  --   branch = 'main',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  -- },
+  -- {
+  --   'nvim-treesitter/nvim-treesitter-context',
+  --   config = function()
+  --     require('treesitter-context').setup {
+  --       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  --       max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  --       min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  --       line_numbers = true,
+  --       multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  --       trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  --       mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+  --       -- Separator between context and content. Should be a single character string, like '-'.
+  --       -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  --       separator = nil,
+  --       zindex = 20, -- The Z-index of the context window
+  --       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+  --     }
+  --   end,
+  -- },
 }
