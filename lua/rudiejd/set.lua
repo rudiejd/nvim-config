2be16bec9d881eab6e2f47c3757d27dc924a2695
JD <jd@rudiejd.io>
2026-07-28 11:41:27 -0400

    feat: try setting cmdheight to 0 with new ui2, add aucmd for displaying macro recording

--- lua/rudiejd/set.lua
+++ lua/rudiejd/set.lua
@@ -87,11 +87,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 --   },
 -- })
 
-vim.filetype.add({
+vim.filetype.add {
   pattern = {
     ['.*csproj'] = 'xml.csproj',
-  }
-})
+  },
+}
 --
 -- todo - figure out how to make this lua
 vim.api.nvim_create_autocmd('BufEnter', {
@@ -162,8 +162,11 @@ vim.api.nvim_create_autocmd('CmdwinEnter', {
 --   end,
 -- })
 
-vim.api.nvim_create_user_command('DiffOrig',
-  'vert new | set buftype=nofile | read ++edit # | 0d_ | diffthis | wincmd p | diffthis', {})
+vim.api.nvim_create_user_command(
+  'DiffOrig',
+  'vert new | set buftype=nofile | read ++edit # | 0d_ | diffthis | wincmd p | diffthis',
+  {}
+)
 
 vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
 vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
@@ -177,8 +180,6 @@ vim.cmd [[
   highlight Normal ctermbg=none
   highlight NonText ctermbg=none
 ]]
-
-
 
 --  completion
 -- vim.opt.completeopt = { "menuone", "noinsert", "popup" }
@@ -196,3 +197,81 @@ vim.cmd [[
 
 -- with ui2, cmd is a floating window. free real estate!
 vim.o.cmdheight = 0
+
+local api = vim.api
+
+---@type integer?
+local win_id = nil
+
+---@type integer?
+local buf_id = nil
+
+---@type { row: integer, col_offset: integer }
+local config = {
+  row = 2,
+  col_offset = 4,
+}
+
+---@return nil
+local function close_banner()
+  if win_id and api.nvim_win_is_valid(win_id) then
+    api.nvim_win_close(win_id, true)
+  end
+
+  if buf_id and api.nvim_buf_is_valid(buf_id) then
+    api.nvim_buf_delete(buf_id, { force = true })
+  end
+
+  win_id = nil
+  buf_id = nil
+end
+
+---@return nil
+local function open_banner()
+  local reg = vim.fn.reg_recording()
+
+  if reg == '' then
+    return
+  end
+
+  close_banner()
+
+  local text = string.format(' ● REC @%s ', reg)
+
+  buf_id = api.nvim_create_buf(false, true)
+  api.nvim_buf_set_lines(buf_id, 0, -1, false, { text })
+
+  local col = vim.o.columns - #text - config.col_offset
+
+  win_id = api.nvim_open_win(buf_id, false, {
+    relative = 'editor',
+    width = #text,
+    height = 1,
+    row = config.row,
+    col = col,
+    style = 'minimal',
+    border = 'none',
+    focusable = false,
+    zindex = 150,
+  })
+
+  api.nvim_set_option_value('winhighlight', 'Normal:DiagnosticError', { win = win_id })
+end
+
+local group = api.nvim_create_augroup('MacroRecordingBanner', { clear = true })
+
+api.nvim_create_autocmd('RecordingEnter', { group = group, callback = open_banner })
+api.nvim_create_autocmd('RecordingLeave', {
+  group = group,
+  callback = function()
+    vim.defer_fn(close_banner, 50)
+  end,
+})
+api.nvim_create_autocmd('VimResized', {
+  group = group,
+  callback = function()
+    if win_id and api.nvim_win_is_valid(win_id) then
+      open_banner()
+    end
+  end,
+})
