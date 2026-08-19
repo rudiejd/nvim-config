e448f6b6ec00873295279373abd6f7c40a12fcc8
JD <jd@rudiejd.io>
2026-08-19 16:08:55 -0400

    feat: add copilot to blink autocomplete

--- lua/rudiejd/plugins/blink.lua
+++ lua/rudiejd/plugins/blink.lua
@@ -1,7 +1,7 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = { 'rafamadriz/friendly-snippets', 'fang2hou/blink-copilot' },
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
@@ -26,23 +26,23 @@ return {
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
-        ['<CR>'] = { 'accept', 'fallback' },
-        ['<C-\\>'] = { 'hide', 'fallback' },
-        ['<C-n>'] = { 'select_next', 'show' },
-        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev' },
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<CR>'] = { 'accept', 'fallback' },
+      ['<C-\\>'] = { 'hide', 'fallback' },
+      ['<C-n>'] = { 'select_next', 'show' },
+      ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev' },
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
     },
 
     signature = {
-      enabled = true
+      enabled = true,
     },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+      nerd_font_variant = 'mono',
     },
 
     completion = { documentation = { auto_show = false } },
@@ -50,7 +50,24 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'copilot', 'path', 'snippets', 'buffer' },
+      providers = {
+        copilot = {
+          name = 'copilot',
+          module = 'blink-copilot',
+          score_offset = 100,
+          async = true,
+          opts = {
+            -- Local options override global ones
+            max_completions = 3, -- Override global max_completions
+
+            -- Final settings:
+            -- * max_completions = 3
+            -- * max_attempts = 2
+            -- * all other options are default
+          },
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -58,7 +75,7 @@ return {
     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
     -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { 'sources.default' },
 }
